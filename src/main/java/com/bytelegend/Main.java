3ceda45a22078e7ec67fc80a12f9d27304f8bb8b
gtn1024 <tony.bazhang@gmail.com>
2022-05-02 13:18:45 +0000

    Update at 2022-05-02T13:18:45.560673Z

--- src/main/java/com/bytelegend/Main.java
+++ src/main/java/com/bytelegend/Main.java
@@ -8,7 +8,7 @@ public class Main {
     public static void main(String[] args) {
         Base object = new Sub();
         ParamBase param = new ParamSub();
-        object.print(param);
+        object.print((ParamSub) param);
     }
 }
 
